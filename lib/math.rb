89f3032b05de4ac7b264848317951aa0d22a7046
Nyasha Perry <nyashaperry2005@yahoo.com>
2020-12-15 07:43:41 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,6 +3,7 @@ def addition(num1, num2)
   sum = num1+num2
   sum
 end
+
 addition(4,5)
 
 def subtraction(num1, num2)
@@ -10,6 +11,7 @@ def subtraction(num1, num2)
   sum = num1-num2
   sum
 end
+
 subtraction(10,5)
 
 def division(num1, num2)
@@ -17,6 +19,7 @@ def division(num1, num2)
   sum = num1/num2
   sum
 end
+
 division(50,2)
 
 def multiplication(num1, num2)
@@ -24,14 +27,21 @@ def multiplication(num1, num2)
   sum = num1*num2
   sum
 end
+
 multiplication(4,30)
 
 def modulo(num1, num2)
   num1%num2
+  sum = num1%num2
+  sum
 end
+
 modulo(34,5)
 
 def square_root(num)
   Math.sqrt(num)
+  sum = Math.sqrt(num)
+  sum
 end
+
 Math.sqrt(81)
