6e200f21a49b17a3c8cc199848576d3e94f9fe46
Nyasha Perry <nyashaperry2005@yahoo.com>
2020-12-15 07:42:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,20 +1,28 @@
 def addition(num1, num2)
   num1+num2
+  sum = num1+num2
+  sum
 end
 addition(4,5)
 
 def subtraction(num1, num2)
   num1-num2
+  sum = num1-num2
+  sum
 end
 subtraction(10,5)
 
 def division(num1, num2)
   num1/num2
+  sum = num1/num2
+  sum
 end
 division(50,2)
 
 def multiplication(num1, num2)
   num1*num2
+  sum = num1*num2
+  sum
 end
 multiplication(4,30)
 
