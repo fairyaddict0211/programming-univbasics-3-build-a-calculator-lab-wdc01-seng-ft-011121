f757009bf8a6b2840244bb486a9461905d3409fb
Nyasha Perry <nyashaperry2005@yahoo.com>
2020-12-15 07:34:57 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,14 +6,17 @@ addition(4,5)
 def subtraction(num1, num2)
   num1 - num2
 end
-subtraction()
+subtraction(10,5)
+
 def division(num1, num2)
   num1/num2
 end
 
+
 def multiplication(num1, num2)
   num1 * num2
 end
+
 
 def modulo(num1, num2)
 num1 % num2
